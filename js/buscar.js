d7deab9cedd33ba4aaa7904fd21b76eb9106e2bf
Ferney Rueda Amaya <135787120+ferruea@users.noreply.github.com>
2023-10-22 17:21:21 -0500

    Add files via upload

---
+++ js/buscar.js
@@ -0,0 +1,24 @@
+//Funcion para crear buscador en una tabla
+function doSearch() {
+                var tableReg = document.getElementById('regTable');
+                var searchText = document.getElementById('searchTerm').value.toLowerCase();
+                for (var i = 0; i < tableReg.rows.length; i++) {
+                    var cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
+                    var found = false;
+                    for (var j = 0; j < cellsOfRow.length && !found; j++) {
+                        var compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+                        if (searchText.length == 0 || (compareWith.indexOf(searchText) > -1)) {
+                            found = true;
+                        }
+                    }
+                    if (found) {
+                        tableReg.rows[i].style.display = '';
+                    } else {
+                        tableReg.rows[i].style.display = 'none';
+                    }
+                }
+            }
+
+function replaceName(e){
+  e.value = e.value.replace(/ /g, "-")
+}
