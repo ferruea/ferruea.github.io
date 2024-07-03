8fd4ef3c07ace8fac5963764f088d8933e64338e
Ferney Rueda Amaya <135787120+ferruea@users.noreply.github.com>
2024-07-02 22:24:08 -0500

    Add files via upload

--- js/buscar.js
+++ js/buscar.js
@@ -19,6 +19,50 @@ function doSearch() {
                 }
             }
 
-function replaceName(e){
-  e.value = e.value.replace(/ /g, "-")
+function replaceName(e) {
+  e.value = e.value.replace(/ /g, "-");
+  e.value = e.value.replace(/ñ/g, 'n');
+  e.value = e.value.replace(/Ñ/g, 'N');
+  e.value = e.value.replace(/[áéíóúÁÉÍÓÚ]/g, function(match) {
+    switch (match) {
+      case 'á': return 'a';
+      case 'é': return 'e';
+      case 'í': return 'i';
+      case 'ó': return 'o';
+      case 'ú': return 'u';
+      case 'Á': return 'A';
+      case 'É': return 'E';
+      case 'Í': return 'I';
+      case 'Ó': return 'O';
+      case 'Ú': return 'U';
+    }
+  });
+  e.value = e.value.replace(/[àèìòùÀÈÌÒÙ]/g, function(match) {
+    switch (match) {
+      case 'à': return 'a';
+      case 'è': return 'e';
+      case 'ì': return 'i';
+      case 'ò': return 'o';
+      case 'ù': return 'u';
+      case 'À': return 'A';
+      case 'È': return 'E';
+      case 'Ì': return 'I';
+      case 'Ò': return 'O';
+      case 'Ù': return 'U';
+    }
+  });
+  e.value = e.value.replace(/[äëïöüÄËÏÖÜ]/g, function(match) {
+    switch (match) {
+      case 'ä': return 'a';
+      case 'ë': return 'e';
+      case 'ï': return 'i';
+      case 'ö': return 'o';
+      case 'ü': return 'u';
+      case 'Ä': return 'A';
+      case 'Ë': return 'E';
+      case 'Ï': return 'I';
+      case 'Ö': return 'O';
+      case 'Ü': return 'U';
+    }
+  });
 }
