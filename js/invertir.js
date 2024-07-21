d62da88c10f4599e51ba4d3f3b598af0fc5b8bf1
Ferney Rueda Amaya <135787120+ferruea@users.noreply.github.com>
2024-07-21 18:14:24 -0500

    Add files via upload

--- js/invertir.js
+++ js/invertir.js
@@ -1,36 +1,23 @@
+//Array invertir colores
 function invertir() { 
     var css = 'html {-webkit-filter: invert(100%);' +
-              '-moz-filter: invert(100%);' + 
-              '-o-filter: invert(100%);' + 
-              '-ms-filter: invert(100%); }' +
-              '@media screen and (max-device-width: 414px) and (orientation: portrait) {' + // iPhone en modo retrato
-              '    .mobile-top-bar {-webkit-filter: invert(100%);' +
-              '                      -moz-filter: invert(100%);' + 
-              '                      -o-filter: invert(100%);' + 
-              '                      -ms-filter: invert(100%); }' +
-              '}';
+        '-moz-filter: invert(100%);' + 
+        '-o-filter: invert(100%);' + 
+        '-ms-filter: invert(100%); }',
+    head = document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
 
-    var head = document.getElementsByTagName('head')[0],
-        style = document.createElement('style');
+if (!window.counter) { window.counter = 1;} else  { window.counter ++;
+if (window.counter % 2 == 0) { var css ='html {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'}
+ };
 
-    if (!window.counter) { 
-        window.counter = 1;
-    } else {  
-        window.counter++;
-        if (window.counter % 2 == 0) { 
-            css = 'html {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }' +
-                  '@media screen and (max-device-width: 414px) and (orientation: portrait) {' +
-                  '    .mobile-top-bar {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }' +
-                  '}';
-        }
-    }
-
-    style.type = 'text/css';
-    if (style.styleSheet){
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-
-    head.appendChild(style);
+style.type = 'text/css';
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
 }
+
+head.appendChild(style);
+ }
+ 
