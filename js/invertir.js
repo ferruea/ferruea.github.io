fb4cb69a008c98295618f7ce80493e4aaebc665d
Ferney Rueda Amaya <135787120+ferruea@users.noreply.github.com>
2024-07-21 18:03:17 -0500

    Add files via upload

--- js/invertir.js
+++ js/invertir.js
@@ -1,35 +1,23 @@
+//Array invertir colores
 function invertir() { 
     var css = 'html {-webkit-filter: invert(100%);' +
         '-moz-filter: invert(100%);' + 
         '-o-filter: invert(100%);' + 
-        '-ms-filter: invert(100%); }';
+        '-ms-filter: invert(100%); }',
+    head = document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
 
-    // Agregar aquí los selectores específicos para las partes superiores e inferiores de los móviles
-    // Ejemplo de selectores:
-    css += 'header, footer {-webkit-filter: invert(100%);' +
-        '-moz-filter: invert(100%);' + 
-        '-o-filter: invert(100%);' + 
-        '-ms-filter: invert(100%); }';
+if (!window.counter) { window.counter = 1;} else  { window.counter ++;
+if (window.counter % 2 == 0) { var css ='html {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'}
+ };
 
-    var head = document.getElementsByTagName('head')[0],
-        style = document.createElement('style');
-
-    if (!window.counter) {
-        window.counter = 1;
-    } else {
-        window.counter++;
-        if (window.counter % 2 == 0) {
-            css = 'html {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }';
-            css += 'header, footer {-webkit-filter: invert(0%); -moz-filter: invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }';
-        }
-    }
-
-    style.type = 'text/css';
-    if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-
-    head.appendChild(style);
+style.type = 'text/css';
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
 }
+
+head.appendChild(style);
+ }
+ 
