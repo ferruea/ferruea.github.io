60b0111d0dd5853a8399bc9fa8e884c6278216cd
Ferney Rueda Amaya <135787120+ferruea@users.noreply.github.com>
2024-08-06 22:19:47 -0500

    Add files via upload

--- js/zoom.js
+++ js/zoom.js
@@ -1,7 +1,14 @@
 // Función para ajustar el zoom
 function adjustZoom() {
-  document.body.style.zoom = '90%';
+  if (window.innerWidth > 1000) {
+    document.body.style.zoom = '90%';
+  } else {
+    document.body.style.zoom = '100%'; // Opcional: restablece el zoom si es menor de 1000px
+  }
 }
 
 // Espera a que el contenido esté completamente cargado
 document.addEventListener('DOMContentLoaded', adjustZoom);
+
+// Ajusta el zoom cuando cambie el tamaño de la ventana
+window.addEventListener('resize', adjustZoom);
