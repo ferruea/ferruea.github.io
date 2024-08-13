95944d5566a86866abf623a309d45a9c93bb4ade
Ferney Rueda Amaya <135787120+ferruea@users.noreply.github.com>
2024-08-12 21:30:50 -0500

    Add files via upload

--- js/zoom.js
+++ js/zoom.js
@@ -6,9 +6,19 @@ function adjustZoom() {
     document.body.style.zoom = '100%'; // Opcional: restablece el zoom si es menor de 1000px
   }
 }
-
 // Espera a que el contenido esté completamente cargado
 document.addEventListener('DOMContentLoaded', adjustZoom);
-
 // Ajusta el zoom cuando cambie el tamaño de la ventana
 window.addEventListener('resize', adjustZoom);
+
+function updateProgressMax() {
+  var progress = document.getElementById('progress');
+  if (window.innerWidth < 1000) {
+    progress.max = 100;
+  } else {
+    progress.max = 90;
+  }
+}
+// Llamar a la función cuando la página se carga y cuando se redimensiona la ventana
+window.onload = updateProgressMax;
+window.onresize = updateProgressMax;
